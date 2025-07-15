e9068b0de1e815bb62fe52dbafc257cd4954d458
luis miguel cruz gareca <lc109070@gmail.com>
2025-07-15 11:19:47 -0400

    modificar em packge

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,12 @@
-import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { CategoryModule } from './category/category.module';
-import { ProductModule } from './product/product.module';
-import { PhotoModule } from './photo/photo.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from "@nestjs/config";
+import { CategoryModule } from "./category/category.module";
+import { PhotoModule } from "./photo/photo.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { ProductModule } from "./product/product.module";
+import { UserModule } from "./user/user.module";
+import { AuthModule } from "./auth/auth.module";
+import { AppService } from "./app.service";
+import { Module } from "@nestjs/common";
 
 @Module({
   imports: [
@@ -16,10 +14,6 @@ import { AuthModule } from './auth/auth.module';
     CategoryModule,
     ProductModule,
     PhotoModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'), // Servir archivos desde 'uploads'
-      serveRoot: '/uploads', // URL base para acceder a los archivos
-    }),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
